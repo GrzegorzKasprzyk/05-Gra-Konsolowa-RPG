d35b92edde80d52f579a9fd5f2ff5ce00b483fd4
GrzegorzKasprzyk <xankrigor87@gmail.com>
2021-07-12 16:08:01 +0200

    modyfied file

--- RPGKonsoleGame/Program.cs
+++ RPGKonsoleGame/Program.cs
@@ -10,6 +10,52 @@ namespace RPGKonsoleGame
     {
         static void Main(string[] args)
         {
+            string liczba1 = Console.ReadLine();
+            string liczba2 = Console.ReadLine();
+
+            int odczytanaLiczba1 = int.Parse(liczba1);
+            int odczytanaLiczba2 = int.Parse(liczba2);
+
+            if (odczytanaLiczba1 < odczytanaLiczba2)
+            {
+                Console.WriteLine("Druga liczba jest większa");
+            }
+            else
+            {
+                Console.WriteLine("Pierwsza liczba jest większa");
+            }
+
+            Console.ReadKey();
+            /*
+            Console.WriteLine("Dokonaj wyboru...");
+
+            string nazwaPostaci;
+            Console.WriteLine("\n1. Nowa Gra");
+            Console.WriteLine("2. Wczytaj Grę");
+            Console.WriteLine("3. Koniec");
+
+            string opcja = Console.ReadLine();
+            Console.WriteLine("Wybrano opcję: " + opcja);
+
+            if (opcja == "1")
+            {
+                Console.Clear();
+                Console.Write("Podaj imię postaci: ");
+                nazwaPostaci = Console.ReadLine();
+            }
+
+            else if (opcja == "2")
+            {
+
+            }
+
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Dzięki za grę");
+            }
+            Console.ReadLine();
+            */
         }
     }
 }
