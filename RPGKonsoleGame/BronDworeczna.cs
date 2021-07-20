b9e1e22a99c6bb513a5c3cdc354864935dd961f0
GrzegorzKasprzyk <xankrigor87@gmail.com>
2021-07-20 16:52:39 +0200

    add file

---
+++ RPGKonsoleGame/BronDworeczna.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGKonsoleGame
+{
+    public class BronDworeczna : IBron
+    {
+        public string Nazwa { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Cena { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int ModyfikatorObrazen { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int ObliczObrazenia()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
